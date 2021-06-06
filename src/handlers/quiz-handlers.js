81df8a0c157568ff46485a5b8e845987f729b524
datmemerboi <shravanssd@gmail.com>
2021-06-06 20:33:19 +0530

    Fix getQuizHandler

--- src/handlers/quiz-handlers.js
+++ src/handlers/quiz-handlers.js
@@ -10,7 +10,7 @@ function createQuizHandler(request, reply) {
   }
   Quiz.create(request.body)
     .then(doc => {
-      reply.status(201).send(doc);
+      reply.code(201).send(doc);
     })
     .catch(e => {
       request.log.error(e);
@@ -22,7 +22,7 @@ function listQuizHandler(request, reply) {
   if (_.has(request.query, 'count') && request.query.count.toLowerCase() === 'true') {
     Quiz.count()
       .then(count => {
-        reply.status(200).send({ total_docs: count });
+        reply.code(200).send({ total_docs: count });
       })
       .catch(e => {
         request.log.error(e);
@@ -32,7 +32,7 @@ function listQuizHandler(request, reply) {
   } else {
     Quiz.list()
       .then(docs => {
-        reply.status(200).send({ total_docs: docs.length, docs });
+        reply.code(200).send({ total_docs: docs.length, docs });
       })
       .catch(e => {
         request.log.error(e);
@@ -45,21 +45,38 @@ function getQuizHandler(request, reply) {
   if (_.isNil(request.params.threeWords)) {
     reply.code(400).send();
   }
-  Quiz.get(request.params.threeWords)
-    .then(doc => {
-      reply.status(200).send(doc);
-    })
-    .catch(e => {
-      request.log.error(e);
-      reply.code(500).send();
-    });
+  if (_.has(request.query, 'linked')) {
+    Quiz.getLinked(request.params.threeWords, request.query.linked.split(','))
+      .then(doc => {
+        if (_.isEmpty(doc)) {
+          reply.code(404).send();
+        }
+        reply.code(200).send(doc);
+      })
+      .catch(e => {
+        request.log.error(e);
+        reply.code(500).send();
+      });
+  } else {
+    Quiz.get(request.params.threeWords)
+      .then(doc => {
+        if (_.isEmpty(doc)) {
+          reply.code(404).send();
+        }
+        reply.code(200).send(doc);
+      })
+      .catch(e => {
+        request.log.error(e);
+        reply.code(500).send();
+      });
+  }
 }
 
 function getQRcodeHandler(request, reply) {
   if (_.isNil(request.params.threeWords)) {
-    reply.status(400).send();
+    reply.code(400).send();
   }
-  reply.status(200).send(generateQuizQRCode(request.params.threeWords));
+  reply.code(200).send(generateQuizQRCode(request.params.threeWords));
 }
 
 function updateQuizHandler(request, reply) {
@@ -68,7 +85,10 @@ function updateQuizHandler(request, reply) {
   }
   Quiz.update(request.params.threeWords, request.body)
     .then(doc => {
-      reply.status(201).send(doc);
+      if (_.isEmpty(doc)) {
+        reply.code(404).send();
+      }
+      reply.code(201).send(doc);
     })
     .catch(e => {
       request.log.error(e);
@@ -80,7 +100,7 @@ function deleteQuizHandler(request, reply) {
   if (_.isNil(request.params.threeWords)) {
     reply.code(400).send();
   }
-  if (_.has(request.query, "purge") && request.query.purge.toLowerCase() === 'true') {
+  if (_.has(request.query, 'purge') && request.query.purge.toLowerCase() === 'true') {
     Quiz.purge(request.params.threeWords)
       .then(() => {
         reply.code(200).send();

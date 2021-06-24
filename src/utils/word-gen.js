b5b8c6f224e633acbe8c2cbd0b44c60d93cb4a66
datmemerboi <shravanssd@gmail.com>
2021-06-24 21:34:16 +0530

    Update word gen utils

--- src/utils/word-gen.js
+++ src/utils/word-gen.js
@@ -3,16 +3,20 @@ const path = require('path');
 const _ = require('lodash');
 
 function generateThreeWords(seperator = "") {
-  let words = fs.readFileSync(path.join(__dirname, '..', '..', 'long_words.txt'), { encoding: 'utf8' });
-  let threeWords = _.chain(words).split('\n').sampleSize(3).value();
-  while (_.uniq(threeWords).length < 2) {
-    threeWords = _.chain(words).split('\n').sampleSize(3).value();
-  }
-  return _.map(threeWords, _.capitalize).join(seperator);
+  let content = fs.readFileSync(path.join(__dirname, '..', '..', 'words', 'combined.txt'), { encoding: 'utf8' });
+  let [verbs, adjs, nouns] = content.split('|');
+  let threeWords = [
+    _.sample(verbs.split(',')),
+    _.sample(adjs.split(',')),
+    _.sample(nouns.split(','))
+  ]
+    .map(_.capitalize)
+    .join(seperator);
+  return threeWords;
 }
 
 function generateMWords(m, seperator = "") {
-  let words = fs.readFileSync(path.join(__dirname, '..', 'long_words.txt'), { encoding: 'utf8' });
+  let words = fs.readFileSync(path.join(__dirname, '..', '..', 'words', 'long_words.txt'), { encoding: 'utf8' });
   let threeWords = _.chain(words).split('\n').sampleSize(m).value();
   while (_.uniq(threeWords).length < (m - 1)) {
     threeWords = _.chain(words).split('\n').sampleSize(m).value();

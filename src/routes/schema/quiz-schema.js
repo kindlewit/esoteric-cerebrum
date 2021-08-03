6921e11e6862afb84df8162e5fe11a4080121bef
datmemerboi <shravanssd@gmail.com>
2021-08-03 21:08:09 +0530

    Move all schema to schema/

---
+++ src/routes/schema/quiz-schema.js
@@ -0,0 +1,134 @@
+const idealObjSchema = {
+  type: 'object',
+  properties: {
+    three_words: { type: 'string' },
+    url: {
+      oneOf: [
+        { type: 'string' },
+        { type: 'null' }
+      ]
+    },
+    title: {
+      oneOf: [
+        { type: 'string' },
+        { type: 'null' }
+      ]
+    },
+    description: {
+      oneOf: [
+        { type: 'string' },
+        { type: 'null' }
+      ]
+    },
+    duration: {
+      oneOf: [
+        { type: 'number' },
+        { type: 'null' }
+      ]
+    },
+    topics: {
+      oneOf: [
+        { type: 'array' },
+        { type: 'null' }
+      ]
+    },
+    file_upload: { type: 'boolean' },
+    is_live: { type: 'boolean' },
+    username: { type: 'string' },
+    created_at: { type: 'string' },
+    updated_at: { type: 'string' }
+  },
+  required: [
+    'three_words',
+    'username'
+  ],
+  additionalProperties: true
+};
+const arrayObjSchema = {
+  type: 'object',
+  properties: {
+    total_docs: { type: 'integer' },
+    docs: { type: 'array' }
+  },
+  additionalProperties: true
+};
+
+export default {
+  createSchema: {
+    body: { type: 'object' },
+    response: {
+      201: idealObjSchema,
+      400: { type: 'null' },
+      401: { type: 'null' },
+      403: { type: 'null' },
+      500: { type: 'null' }
+    }
+  },
+  listSchema: {
+    querystring: {
+      page: { type: 'integer' },
+      size: { type: 'integer' },
+      count: { type: 'boolean' }
+    },
+    response: {
+      200: arrayObjSchema,
+      400: { type: 'null' },
+      404: { type: 'null' },
+      500: { type: 'null' }
+    },
+    additionalProperties: true
+  },
+  getSchema: {
+    params: {
+      threeWords: { type: 'string' }
+    },
+    querystring: {
+      linked: { type: 'string' }
+    },
+    response: {
+      200: idealObjSchema,
+      400: { type: 'null' },
+      404: { type: 'null' },
+      500: { type: 'null' }
+    }
+  },
+  qrSchema: {
+    params: {
+      threeWords: { type: 'string' }
+    },
+    response: {
+      200: { type: 'string' },
+      400: { type: 'null' },
+      500: { type: 'null' }
+    }
+  },
+  updateSchema: {
+    params: {
+      threeWords: { type: 'string' }
+    },
+    body: { type: 'object' },
+    response: {
+      200: idealObjSchema,
+      400: { type: 'null' },
+      401: { type: 'null' },
+      403: { type: 'null' },
+      404: { type: 'null' },
+      500: { type: 'null' }
+    }
+  },
+  deleteSchema: {
+    params: {
+      threeWords: { type: 'string' }
+    },
+    querystring: {
+      purge: { type: 'string' }
+    },
+    response: {
+      204: { type: 'null' },
+      401: { type: 'null' },
+      403: { type: 'null' },
+      400: { type: 'null' },
+      500: { type: 'null' }
+    }
+  }
+};
---
+++ src/routes/schema/user-schema.js
@@ -0,0 +1,114 @@
+const idealObjSchema = {
+  type: 'object',
+  properties: {
+    username: { type: 'string' },
+    email: { type: 'string' },
+    display_name: {
+      oneOf: [
+        { type: 'string' },
+        { type: 'null' }
+      ]
+    },
+    attended: {
+      oneOf: [
+        { type: 'null' },
+        { type: 'array' }
+      ]
+    },
+    created_at: { type: 'string' }
+  },
+  required: [
+    'username'
+  ],
+  additionalProperties: true
+};
+const arrayObjSchema = {
+  type: 'object',
+  properties: {
+    total_docs: { type: 'number' },
+    docs: { type: 'array' }
+  },
+  additionalProperties: true
+};
+
+export default {
+  signupSchema: {
+    body: {
+      username: { type: 'string' },
+      password: { type: 'string' },
+      email: { type: 'string' }
+    },
+    response: {
+      201: idealObjSchema,
+      400: { type: 'null' },
+      500: { type: 'null' }
+    }
+  },
+  listSchema: {
+    querystring: {
+      count: { type: 'boolean' },
+      limit: { type: 'integer' },
+      offset: { type: 'integer' }
+    },
+    response: {
+      200: arrayObjSchema,
+      500: { type: 'null' }
+    }
+  },
+  getSchema: {
+    params: {
+      username: { type: 'string' }
+    },
+    querystring: {
+      linked: { type: 'string' }
+    },
+    response: {
+      200: idealObjSchema,
+      400: { type: 'null' },
+      404: { type: 'null' },
+      500: { type: 'null' }
+    }
+  },
+  loginSchmea: {
+    body: {
+      username: { type: 'string' },
+      password: { type: 'string' }
+    },
+    response: {
+      200: { type: 'null' },
+      400: { type: 'null' },
+      401: { type: 'null' },
+      404: { type: 'null' },
+      500: { type: 'null' }
+    }
+  },
+  updateSchema: {
+    params: {
+      username: { type: 'string' }
+    },
+    body: { type: 'object' },
+    response: {
+      200: idealObjSchema,
+      400: { type: 'null' },
+      401: { type: 'null' },
+      403: { type: 'null' },
+      404: { type: 'null' },
+      500: { type: 'null' }
+    }
+  },
+  deleteSchema: {
+    params: {
+      username: { type: 'string' }
+    },
+    querystring: {
+      purge: { type: 'boolean' }
+    },
+    response: {
+      204: { type: 'null' },
+      400: { type: 'null' },
+      401: { type: 'null' },
+      403: { type: 'null' },
+      500: { type: 'null' }
+    }
+  }
+};

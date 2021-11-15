ab078a36b4cbfe0a028f9c28eef1dbf498b7b804
datmemerboi <shravanssd@gmail.com>
2021-11-15 21:14:18 +0530

    Minor Changes & Comments
    - Changed constant name to ES_API_VERSION
    - Add comments for future versions & TODOs

--- src/utils/logger-utils.js
+++ src/utils/logger-utils.js
@@ -3,16 +3,17 @@
 import pino from 'pino';
 import pinoES from 'pino-elasticsearch';
 
-import { ES_HOST, ES_API_VER } from '../config';
+import { ES_HOST, ES_API_VERSION } from '../config';
 
 const apiV1LogStream = pinoES({
-  index: 'api-v1-log-2021',
+  index: 'api-v1-log-2021', // change index to log-YYYY-month
   consistency: 'one',
   node: ES_HOST,
-  'es-version': parseInt(ES_API_VER),
+  'es-version': parseInt(ES_API_VERSION),
   'flush-bytes': 1000
 });
 
+// TODO: Config log as per https://github.com/pinojs/pino/blob/master/docs/api.md#log
 const apiV1Logger = pino(
   {
     level: 'info',

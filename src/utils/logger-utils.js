5ec698807b19ab3cffba0db24b6707a6cb768d10
datmemerboi <shravanssd@gmail.com>
2021-12-15 16:21:15 +0530

    Update logger util

--- src/utils/logger-utils.js
+++ src/utils/logger-utils.js
@@ -14,10 +14,38 @@ const apiV1LogStream = pinoES({
 });
 
 // TODO: Config log as per https://github.com/pinojs/pino/blob/master/docs/api.md#log
+/*
 const apiV1Logger = pino(
   {
     level: 'info',
     redact: ['req.headers.authorization']
+  },
+  apiV1LogStream
+);
+*/
+function modifyLog(log) {
+  if (log.req) {
+    // console.log(log.req.raw.url, log.req.raw);
+    return {
+      abc: 'jkl',
+      method: log.req.raw.method,
+      url: log.req.raw.url,
+      httpVersion: log.req.raw.httpVersion,
+      rawHeaders: log.req.raw.rawHeaders,
+      statusCode: log.req.raw.statusCode,
+      statusMessage: log.req.raw.statusMessage,
+      keepAliveTimeout: log.req.raw.socket.server.keepAliveTimeout
+    };
+  }
+  return log;
+}
+
+const apiV1Logger = pino(
+  {
+    formatters: {
+      log: modifyLog
+    },
+    level: 'info'
   },
   apiV1LogStream
 );

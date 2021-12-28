9ff6278068f55767e22b9067decdcc0f9f62c2c1
datmemerboi <shravanssd@gmail.com>
2021-12-28 15:02:41 +0530

    Update ES utils

--- src/utils/elasticsearch-utils.js
+++ src/utils/elasticsearch-utils.js
@@ -1,5 +1,13 @@
+/**
+ * Creating custom ES actor as custom mappings is not available in Pino
+ *
+ * As of Dec 28, 2021: logging works perfectly for simple queries.
+ * Checks required for complications
+ */
+'use strict';
+
 import { Client } from '@elastic/elasticsearch';
-import { Transform } from 'stream';
+import { LOG_MAPPING } from '../config';
 
 export function ElasticSearchUtils(opts) {
   this.client = new Client({
@@ -7,7 +15,7 @@ export function ElasticSearchUtils(opts) {
     auth: null,
     cloud: null
   });
-  this.index = opts?.index ?? null;
+  this.index = opts.index ?? null;
 }
 
 ElasticSearchUtils.prototype.indexExists = function (name) {
@@ -22,89 +30,41 @@ ElasticSearchUtils.prototype.indexExists = function (name) {
   });
 };
 
-ElasticSearchUtils.prototype.createIndex = function (index, mappings = null) {
-  return new Promise((resolve, reject) => {
-    let body = {};
-    if (mappings !== null || mappings !== undefined) {
-      body.mappings = mappings;
-    }
-    return this.client.indices
-      .create(
-        {
-          index,
-          body
-        },
-        { ignore: [400] }
-      )
-      .then((d) => resolve(d))
-      .catch(() => reject(false));
-  });
-};
-
-ElasticSearchUtils.prototype.client = function () {
-  return this.client;
-};
-
-ElasticSearchUtils.prototype.setIndex = function (index) {
+ElasticSearchUtils.prototype.setIndex = async function (index, mapping) {
+  if (!this.client.indices.exists({ index })) {
+    let body = {
+      mappings:
+        mapping !== undefined && mapping !== null ? mapping : LOG_MAPPING
+    };
+    await this.client.indices.create(
+      {
+        index,
+        body
+      },
+      { ignore: [400] }
+    );
+  }
   this.index = index;
   return true;
 };
 
-ElasticSearchUtils.prototype.createLogStream = function () {
-  let { index, client } = this;
+ElasticSearchUtils.prototype.write = async function (line) {
+  let body;
+  try {
+    body = { ...JSON.parse(line) };
 
-  return new Transform({
-    readableObjectMode: true,
-    write: (chunk, encoding, next) => {
-      let body;
-
-      try {
-        body = JSON.parse(chunk);
-        body.timestamp = new Date();
-      } catch (e) {
-        next();
-      }
-      console.log(body);
-      client
-        .index({
-          index,
-          body
-        })
-        .then(() => next);
-    }
+    // Push below loc into log_formatter fn
+    if (Object.prototype.hasOwnProperty.call(body, 'time')) delete body.time;
+    body.timestamp = new Date().valueOf();
+  } catch {
+    body = {
+      value: line,
+      timestamp: new Date().valueOf()
+    };
+  }
+  await this.client.index({
+    index: this.index,
+    body
   });
+  return;
 };
-
-ElasticSearchUtils.prototype.bulkIndex = function (data) {
-  return new Promise((resolve, reject) => {
-    if (
-      this.client === null ||
-      this.index === undefined ||
-      this.index === null
-    ) {
-      reject(false);
-    }
-    if (data === null || typeof data !== 'object') {
-      reject(false);
-    }
-    this.client.helpers
-      .bulk({
-        datasource: data,
-        onDocument(doc) {
-          doc.timestamp = new Date();
-          return {
-            index: { _index: this.index }
-          };
-        }
-      })
-      .then(() => resolve(true))
-      .catch(() => reject(false));
-  });
-};
-
-/**
- * Creating custom ES actor as custom mappings is not available in Pino
- *
- * As of Dec 26, 2021: logging works perfectly for Server start
- * but not for individual API calls
- */
